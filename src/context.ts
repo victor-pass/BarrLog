209fbc35be9f4ad1ee41bd844ff42e92281b6087
Stephen Smithwick <stephen.smithwick@gmail.com>
2026-09-18 10:25:11 -0600

    renaming context.tsx to context.ts as there is no TSX in the module

--- src/context.ts
+++ src/context.ts
@@ -11,6 +11,6 @@ export const AppContext = createContext<AppContextValue>();
 
 export function context(): AppContextValue {
   const value = useContext(AppContext);
-  if (!value) throw new Error("context must be used within App context");
+  if (!value) throw new Error("context must be used within App");
   return value;
 }
