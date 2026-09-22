f39b50f3b1fdc99b5159dd1bd32a6b86ba7facff
Stephen Smithwick <stephen.smithwick@gmail.com>
2026-09-22 03:23:49 -0600

    InMemoryDB now uses a snapshot for faster tests (initial test is still slow)

--- src/test/inMemoryDB.ts
+++ src/test/inMemoryDB.ts
@@ -4,13 +4,20 @@ import { relations } from "@/../drizzle/relations";
 import { type LoadDB } from "@/db";
 import { PGlite } from "@electric-sql/pglite";
 
+let snapshot: File | Blob | undefined = undefined;
 export const inMemoryDB: LoadDB = async () => {
+  if (snapshot) {
+    const client = new PGlite({ loadDataDir: snapshot });
+    return drizzle({ client, relations });
+  }
+
   const client = new PGlite();
   const db = drizzle({ client, relations });
 
   await migrate(db, {
     migrationsFolder: "./drizzle",
   });
+  snapshot = await client.dumpDataDir("none");
 
   return db;
 };
